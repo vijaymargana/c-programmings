d5d85aa6a62b8cdd06143bae68583f1a2862e8ae
vijaymargana <vijaymargana353@gmail.com>
2025-10-27 18:02:06 +0530

    Add files via upload

---
+++ ifex.c
@@ -0,0 +1,13 @@
+/*program for the simple if*/
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the your age:");
+	scanf("%d",&a);
+	if(a>=18){
+	printf("you are eligible for the vote");
+	}
+	return 0;
+
+}
