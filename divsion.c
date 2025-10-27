386c8a5b82d98f229f751d748b881be627141bf8
vijaymargana <vijaymargana353@gmail.com>
2025-10-27 17:15:44 +0530

    Add files via upload

---
+++ divsion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	float div;
+	printf("enter the two numbers for division:");
+	scanf("%d%d",&a,&b);
+	div=a/b;
+	printf("division is %.2f",div);
+	return 0;
+}
